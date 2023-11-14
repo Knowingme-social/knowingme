02d60a4a2a88bc6f5cc233373b64aa88f55a1531
jackymomomo <jacklloydrobinson@gmail.com>
2023-11-14 01:45:41 -0800

    merged navbar with main

--- screens/WhoKnowsWho.tsx
+++ screens/WhoKnowsWho.tsx
@@ -1,13 +1,14 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable @typescript-eslint/no-shadow */
 /* eslint-disable no-catch-shadow */
-import React, {useEffect, useState} from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
+import React, {useEffect, useState, useRef} from 'react';
+import {View, Text, StyleSheet, Button, Animated, TouchableOpacity} from 'react-native';
 import {collection, getDocs, query, where} from 'firebase/firestore';
 import {FIREBASE_AUTH, FIRESTORE_DB} from '../firebaseConfig';
 import {onAuthStateChanged} from 'firebase/auth';
 
 export default function WhoKnowsWho({navigation}) {
+  const [selectedTab, setSelectedTab] = useState('bestKnownByYou');
   const [user, setUser] = useState(null);
   const [bestFriend, setBestFriend] = useState('');
   const [leastKnownFriend, setLeastKnownFriend] = useState('');
@@ -15,6 +16,24 @@ export default function WhoKnowsWho({navigation}) {
   const [friendStatistics, setFriendStatistics] = useState({});
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState('');
+
+  const animation = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.spring(animation, {
+      toValue: selectedTab === 'bestKnownByYou' ? 0 : 1,
+      useNativeDriver: true,
+    }).start();
+  }, [selectedTab, animation]);
+ 
+  const slideInterpolate = animation.interpolate({
+    inputRange: [0, 1],
+    outputRange: [-69, 74], // Adjust this based on your tab's width
+  });
+ 
+  const animatedIndicatorStyle = {
+    transform: [{ translateX: slideInterpolate }],
+  };
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(FIREBASE_AUTH, currentUser => {
@@ -33,6 +52,8 @@ export default function WhoKnowsWho({navigation}) {
     }
   }, [user]);
 
+
+  
   const resetStates = () => {
     setBestFriend('');
     setLeastKnownFriend('');
@@ -171,21 +192,120 @@ export default function WhoKnowsWho({navigation}) {
     ));
   };
 
+  const renderContent = () => {
+    switch (selectedTab) {
+      case 'bestKnownByYou':
+        return <Text>you know {bestFriend} the best</Text>;
+        {renderStatistics()}
+        <Text>Statistics for 'Best Known you'</Text>
+      case 'bestKnownByOthers':
+        return <Text> {mostKnowledgeableFriend} knows you the best</Text>;
+        {renderStatistics()}
+        <Text>Statistics for 'Best Known By Others'</Text>
+        case 'leastKnownByOthers':
+        return <Text> {leastKnownFriend} knows you the worst they suck</Text>;
+        {renderStatistics()}
+        <Text>Statistics for 'Least Known By Others'</Text>
+      default:
+        return null;
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>You know {bestFriend} the best!</Text>
-      <Text>You know {leastKnownFriend} the least.</Text>
-      <Text>{mostKnowledgeableFriend} knows you the best!</Text>
-      {renderStatistics()}
-      <Button title="Go Back" onPress={() => navigation.pop()} />
+    <View style={styles.card}>
+      <View style={styles.tabs}>
+        {/* The tabs themselves stay the same color */}
+        <TouchableOpacity
+          style={[styles.tab, styles.leftTab]}
+          onPress={() => setSelectedTab('bestKnownByYou')}
+        >
+          <Text style={[styles.tabText, selectedTab === 'bestKnownByYou' && styles.activeTabText]}>Who You Know</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.tab, styles.rightTab]}
+          onPress={() => setSelectedTab('bestKnownByOthers')}
+        >
+          <Text style={[styles.tabText, selectedTab === 'bestKnownByOthers' && styles.activeTabText]}>Who Knows You</Text>
+        </TouchableOpacity>
+        {/* <TouchableOpacity
+          style={[styles.tab, styles.leftTab]}
+          onPress={() => setSelectedTab('leastKnownByOthers')}
+        >
+          <Text style={[styles.tabText, selectedTab === 'bestKnownByYou' && styles.activeTabText]}>Who You Know the worst</Text>
+        </TouchableOpacity> */}
+        {/* Animated indicator */}
+        <Animated.View style={[styles.indicator, animatedIndicatorStyle]} />
+      </View>
+      <View style={styles.content}>
+        {renderContent()}
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
+  slider: {
+    position: 'absolute',
+    height: 20,
+    width: '50%', // Assuming each tab is half the width of the container
+    backgroundColor: 'black', // Active tab color
+    borderRadius: 20, // Match your tabs' borderRadius
+    // Add other styling if needed
+  },
+  card: {
+    backgroundColor: '#fff',
+    fontFamily: 'Times New Roman',
+    // marginHorizontal: 20, // Added horizontal margin to provide space on both sides
+    marginTop: 100,
+    shadowColor: '#000', // Added shadow for a nice elevation effect
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    height: 700,
+    overflow: 'hidden',
+    zIndex: -2, 
+  },
+  tabs: {
+    flexDirection: 'row',
+    justifyContent: 'center', // Adjusted for center alignment
+    backgroundColor: '#fff', // Corrected color from '#white' to '#fff'
+    borderRadius: 30,
+    overflow: 'hidden',
+    marginHorizontal: 60,
+  },
+  tab: {
     flex: 1,
-    justifyContent: 'center',
+    paddingVertical: 45,
     alignItems: 'center',
+    justifyContent: 'center',
+    // Tabs have their default color
+    backgroundColor: 'grey',
+  },
+  activeTabText: {
+    color: 'white', // Text color for active tab
+    fontWeight: 'bold',
+  },
+  tabText: {
+    color: '#ffe0de', // Default tab text color
+  },
+  indicator: {
+    position: 'absolute',
+    bottom: 0,
+    height: 5,
+    width: '50%', // Set this to match the width of your tabs
+    backgroundColor: 'black', // Indicator color
+    borderRadius: 2,
+  },
+  leftTab: {
+    borderTopLeftRadius: 20, // Rounded corners for the left tab
+    borderBottomLeftRadius: 20,
+  },
+  rightTab: {
+    borderTopRightRadius: 20, // Rounded corners for the right tab
+    borderBottomRightRadius: 20,
+  },
+  content: {
+    padding: 20,
   },
 });
